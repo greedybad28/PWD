890b8fb3720cc67143e508433107cf3bf1079fcb
REmoo <remoantony32@gmail.com>
2024-06-05 11:51:15 +0530

    new changes

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,12 @@
 self.addEventListener('fetch', function(event) {
     // This will eventually handle offline requests, but for now does nothing
   });
+
+const CACHE_NAME = "my-pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+];
+self.addEventListener("")
