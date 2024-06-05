7feb861731a219a010473ad0115ba33971544ec9
REmoo <remoantony32@gmail.com>
2024-06-06 00:44:42 +0530

    Edit and checkout

--- service-worker.js
+++ service-worker.js
@@ -9,4 +9,26 @@ const urlsToCache = [
   "/styles.css",
   "/script.js",
 ];
-self.addEventListener("")
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => {
+      console.log("Opened Cache")
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request)
+    })
+  )
+})
+// 
+// 
+// 
